01d4978670f0ecf40795e4ee5a1d676665eb2adb
j3dsantos <jv.dossantos012@gmail.com>
2022-07-14 16:55:27 -0300

    projeto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
-const pom = document.querySelector('.tecla_pom');
-pom.addEventListener('click', function () {
-    const somPom = document.querySelector('#som_tecla_pom');
-    somPom.play();
-});
+function tocaSom (idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+
+    if(elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+};
+
+const teclas = document.querySelectorAll('.tecla');
+
+for(let i = 0; i < teclas.length; i++) {
+    const tecla = teclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        };
+    };
+
+    tecla.onkeyup = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.remove('ativa');
+        };
+    };
+};
